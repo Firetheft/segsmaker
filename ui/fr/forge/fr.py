d8597b1640e84cbd6796f77732648e59c4ccb420
Firetheft <disambo1985@gmail.com>
2024-03-02 22:42:05 +0800

    2024030201

---
+++ ui/fr/forge/fr.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+
+minyak = [
+    ['rm', '-rf', '~/tmp/*', '~/tmp', '~/stable-diffusion-webui-forge/models/Stable-diffusion/tmp_models', '~/stable-diffusion-webui-forge/models/Lora/tmp_Lora', '~/stable-diffusion-webui-forge/models/ControlNet'],
+    ['mkdir', '-p', '~/stable-diffusion-webui-forge/models/Lora'],
+    ['mkdir', '-p', '~/stable-diffusion-webui-forge/models/ESRGAN'],
+    ['ln', '-vs', '/tmp', '~/tmp'],
+    ['ln', '-vs', '/tmp/models', '~/stable-diffusion-webui-forge/models/Stable-diffusion/tmp_models'],
+    ['ln', '-vs', '/tmp/Lora', '~/stable-diffusion-webui-forge/models/Lora/tmp_Lora'],
+    ['ln', '-vs', '/tmp/ControlNet', '~/stable-diffusion-webui-forge/models/ControlNet']
+]
+
+for tepung in minyak:
+    gorengan = [os.path.expanduser(arg) for arg in tepung]
+    subprocess.run(gorengan, check=True)
