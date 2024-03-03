efc1a3d7091b93d9d63e090d8812d08590e8a764
Firetheft <disambo1985@gmail.com>
2024-03-03 12:33:09 +0800

    2024030311

--- ui/fr/forge/fr.py
+++ ui/fr/forge/fr.py
@@ -8,7 +8,7 @@ minyak = [
     ['ln', '-vs', '/tmp', '~/tmp'],
     ['ln', '-vs', '/tmp/models', '~/stable-diffusion-webui-forge/models/Stable-diffusion/tmp_models'],
     ['ln', '-vs', '/tmp/Lora', '~/stable-diffusion-webui-forge/models/Lora/tmp_Lora'],
-    ['ln', '-vs', '/tmp/ControlNet', '~/stable-diffusion-webui-forge/models/ControlNet']
+    ['ln', '-vs', '/tmp/ControlNet', '~/stable-diffusion-webui-forge/models/ControlNet'],
     ['ln', '-vs', '/tmp/svd', '~/stable-diffusion-webui-forge/models/svd/tmp_svd']
 ]
 
