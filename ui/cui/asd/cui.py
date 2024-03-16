1f4b78dddb640403cf06cec69b18b9e68c9a80e5
Firetheft <disambo1985@gmail.com>
2024-03-16 23:02:55 +0800

    2024031602

--- ui/cui/asd/cui.py
+++ ui/cui/asd/cui.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 
 minyak = [
-    ['rm', '-rf', '~/tmp/*', '~/tmp', '~/ComfyUI/models/checkpoints', '~/ComfyUI/models/loras', '~/ComfyUI/models/controlnet'],
+    ['rm', '-rf', '~/tmp/*', '~/tmp', '~/ComfyUI/models/checkpoints/tmp_models', '~/ComfyUI/models/loras/tmp_Lora', '~/ComfyUI/models/controlnet'],
     ['ln', '-vs', '/tmp', '~/tmp'],
-    ['ln', '-vs', '/tmp/models', '~/ComfyUI/models/checkpoints'],
-    ['ln', '-vs', '/tmp/Lora', '~/ComfyUI/models/loras'],
+    ['ln', '-vs', '/tmp/models', '~/ComfyUI/models/checkpoints/tmp_models'],
+    ['ln', '-vs', '/tmp/Lora', '~/ComfyUI/models/loras/tmp_Lora'],
     ['ln', '-vs', '/tmp/ControlNet', '~/ComfyUI/models/controlnet'],
     ['unzip', '-o', '~/ComfyUI/models/embeddings.zip', '-d', '~/ComfyUI/models/embeddings'],
     ['rm', '~/ComfyUI/models/embeddings.zip']
